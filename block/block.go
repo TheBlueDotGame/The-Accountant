445a5d99644c6d66141b9070889f84591e436e67
Bartosz Lenart <lenart.consulting@gmail.com>
2023-04-13 12:22:22 +0200

    Impl merkle tree for stronger transactions hash and proof of work.

--- block/block.go
+++ block/block.go
@@ -1,48 +1,209 @@
 package block
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"encoding/binary"
+	"math"
+	"math/big"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+var separator = []byte{}
+
 // Block holds block information.
 type Block struct {
-	ID        primitive.ObjectID `json:"-"          bson:"_id"`
-	Index     uint64             `json:"index"      bson:"index"`
-	Timestamp uint64             `json:"timestamp"  bson:"timestamp"`
-	Hash      [32]byte           `json:"hash"       bson:"hash"`
-	PrevHash  [32]byte           `json:"prevHash"   bson:"prevHash"`
-	TrxHashes [][32]byte         `json:"trx_hashes" bson:"trx_hashes"`
+	ID         primitive.ObjectID `json:"-"          bson:"_id"`
+	Index      uint64             `json:"index"      bson:"index"`
+	Timestamp  uint64             `json:"timestamp"  bson:"timestamp"`
+	Nonce      uint64             `json:"nonce"      bson:"nonce"`
+	Difficulty uint64             `json:"difficulty" bson:"difficulty"`
+	Hash       [32]byte           `json:"hash"       bson:"hash"`
+	PrevHash   [32]byte           `json:"prevHash"   bson:"prevHash"`
+	TrxHashes  [][32]byte         `json:"trx_hashes" bson:"trx_hashes"`
 }
 
 // NewBlock creates a new block.
-func NewBlock(next uint64, prevHash [32]byte, trxHashes [][32]byte) Block {
+func NewBlock(difficulty, next uint64, prevHash [32]byte, trxHashes [][32]byte) Block {
 	ts := uint64(time.Now().UnixNano())
 
 	block := Block{
-		ID:        primitive.NilObjectID,
-		Index:     next,
-		Timestamp: ts,
-		Hash:      [32]byte{},
-		PrevHash:  prevHash,
-		TrxHashes: trxHashes,
+		ID:         primitive.NilObjectID,
+		Index:      next,
+		Timestamp:  ts,
+		Nonce:      0,
+		Difficulty: difficulty,
+		Hash:       [32]byte{},
+		PrevHash:   prevHash,
+		TrxHashes:  trxHashes,
 	}
-	block.calculateHash()
+
+	trxHash := block.hashTrxs()
+
+	proof := newProof(&block)
+	block.Nonce, block.Hash = proof.run(trxHash)
 
 	return block
 }
 
-func (b *Block) calculateHash() {
-	var data []byte
-	binary.LittleEndian.AppendUint64(data, b.Index)
-	binary.LittleEndian.AppendUint64(data, b.Timestamp)
-	data = append(data, b.PrevHash[:]...)
-	for _, trx := range b.TrxHashes {
-		data = append(data, trx[:]...)
+// Validate validates block.
+func (b *Block) Validate(trxHashes [][32]byte) bool {
+	if !b.validateTransactionsHashesMatch(trxHashes) {
+		return false
+	}
+	trxHash := b.hashTrxs()
+	proof := newProof(b)
+	return proof.validate(trxHash)
+}
+
+func (b *Block) validateTransactionsHashesMatch(trxHashes [][32]byte) bool {
+	if len(trxHashes) != len(b.TrxHashes) {
+		return false
 	}
 
-	b.Hash = sha256.Sum256(data)
+	set := make(map[[32]byte]struct{})
+	for _, h := range b.TrxHashes {
+		set[h] = struct{}{}
+	}
+
+	if len(set) != len(b.TrxHashes) {
+		return false
+	}
+
+	for _, h := range trxHashes {
+		if _, ok := set[h]; !ok {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (b *Block) hashTrxs() [32]byte {
+	merkle := newMerkleTree(b.TrxHashes)
+	if merkle == nil {
+		return [32]byte{}
+	}
+	return merkle.rootNode.hash
+}
+
+type merkleTree struct {
+	rootNode *merkleNode
+}
+
+type merkleNode struct {
+	left  *merkleNode
+	right *merkleNode
+	hash  [32]byte
+}
+
+func newMerkleNode(left, right *merkleNode, hash [32]byte) *merkleNode {
+	node := merkleNode{}
+
+	switch {
+	case left == nil && right == nil:
+		node.hash = hash
+	default:
+		prevHashes := append(left.hash[:], right.hash[:]...)
+		hash := sha256.Sum256(prevHashes)
+		node.hash = hash
+	}
+
+	node.left = left
+	node.right = right
+
+	return &node
+}
+
+func newMerkleTree(hashes [][32]byte) *merkleTree {
+	if len(hashes) == 0 {
+		return nil
+	}
+
+	nodes := make([]merkleNode, 0, len(hashes))
+	for _, d := range hashes {
+		node := newMerkleNode(nil, nil, d)
+		nodes = append(nodes, *node)
+	}
+
+	for len(nodes) > 1 {
+		if len(nodes)%2 != 0 {
+			nodes = append(nodes, nodes[len(nodes)-1])
+		}
+
+		level := make([]merkleNode, 0, len(nodes)/2)
+		for i := 0; i < len(nodes); i += 2 {
+			node := newMerkleNode(&nodes[i], &nodes[i+1], [32]byte{})
+			level = append(level, *node)
+		}
+		nodes = level
+	}
+
+	tree := merkleTree{&nodes[0]}
+	return &tree
+}
+
+type proofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+func newProof(b *Block) *proofOfWork {
+	if b == nil {
+		return nil
+	}
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-b.Difficulty))
+
+	pow := &proofOfWork{b, target}
+
+	return pow
+}
+
+func (pow *proofOfWork) initData(nonce uint64, h [32]byte) []byte {
+	blockData := make([]byte, 0, 32) // to fit four uint64
+	blockData = binary.LittleEndian.AppendUint64(blockData, nonce)
+	blockData = binary.LittleEndian.AppendUint64(blockData, pow.block.Difficulty)
+	blockData = binary.LittleEndian.AppendUint64(blockData, pow.block.Index)
+	blockData = binary.LittleEndian.AppendUint64(blockData, pow.block.Timestamp)
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevHash[:],
+			h[:],
+			blockData,
+		},
+		separator,
+	)
+
+	return data
+}
+
+func (pow *proofOfWork) run(trxHash [32]byte) (uint64, [32]byte) {
+	var intHash big.Int
+	var hash [32]byte
+	var nonce uint64
+
+	for nonce <= math.MaxUint64 {
+		data := pow.initData(nonce, trxHash)
+		hash = sha256.Sum256(data)
+		intHash.SetBytes(hash[:])
+
+		if intHash.Cmp(pow.target) == -1 {
+			break
+		}
+		nonce++
+	}
+	return nonce, hash
+}
+
+// Validate validates proof of work
+func (pow *proofOfWork) validate(trxHash [32]byte) bool {
+	var intHash big.Int
+	data := pow.initData(pow.block.Nonce, trxHash)
+	hash := sha256.Sum256(data)
+	intHash.SetBytes(hash[:])
+
+	return intHash.Cmp(pow.target) == -1
 }
--- block/block_test.go
+++ block/block_test.go
@@ -10,7 +10,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBlock(t *testing.T) {
+func TestBlockCreate(t *testing.T) {
+	difficulty := uint64(1)
+
 	issuer, err := wallet.New()
 	assert.Nil(t, err)
 	receiver, err := wallet.New()
@@ -26,7 +28,7 @@ func TestBlock(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotEmpty(t, trxHash)
 
-	blc := NewBlock(0, [32]byte{}, [][32]byte{trxHash})
+	blc := NewBlock(difficulty, 0, [32]byte{}, [][32]byte{trxHash})
 
 	assert.NotEmpty(t, blc.Hash)
 
@@ -42,7 +44,7 @@ func TestBlock(t *testing.T) {
 
 		ntrxHash, err := ntrx.Sign(&receiver, verifier)
 		assert.Nil(t, err)
-		newBlck := NewBlock(uint64(i), blockchain[i-1].Hash, [][32]byte{ntrxHash})
+		newBlck := NewBlock(difficulty, uint64(i), blockchain[i-1].Hash, [][32]byte{ntrxHash})
 		blockchain = append(blockchain, newBlck)
 	}
 
@@ -54,7 +56,105 @@ func TestBlock(t *testing.T) {
 	}
 }
 
+func TestBlockValidateSuccess(t *testing.T) {
+	difficulty := uint64(1)
+
+	issuer, err := wallet.New()
+	assert.Nil(t, err)
+	receiver, err := wallet.New()
+	assert.Nil(t, err)
+
+	verifier := wallet.Helper{}
+
+	message := []byte("genesis transaction")
+	trx, err := transaction.New("genesis", message, &issuer)
+	assert.Nil(t, err)
+
+	trxHash, err := trx.Sign(&receiver, verifier)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, trxHash)
+
+	blc := NewBlock(difficulty, 0, [32]byte{}, [][32]byte{trxHash})
+
+	assert.NotEmpty(t, blc.Hash)
+
+	blocksNum := 10000
+	blockchain := make([]Block, 0, blocksNum)
+	blockchain = append(blockchain, blc)
+
+	for i := 1; i <= blocksNum; i++ {
+		nextMsg := []byte(fmt.Sprintf("next message: %v", i))
+
+		ntrx, err := transaction.New("text", nextMsg, &issuer)
+		assert.Nil(t, err)
+
+		ntrxHash, err := ntrx.Sign(&receiver, verifier)
+		assert.Nil(t, err)
+		newBlck := NewBlock(difficulty, uint64(i), blockchain[i-1].Hash, [][32]byte{ntrxHash})
+		blockchain = append(blockchain, newBlck)
+	}
+
+	prevHash := blockchain[len(blockchain)-1].PrevHash
+	for j := len(blockchain) - 2; j >= 0; j-- {
+		ok := bytes.Equal(prevHash[:], blockchain[j].Hash[:])
+		assert.True(t, ok)
+		prevHash = blockchain[j].PrevHash
+		ok = blockchain[j].Validate(blockchain[j].TrxHashes)
+		assert.True(t, ok)
+	}
+}
+
+func TestBlockValidateFailure(t *testing.T) {
+	difficulty := uint64(1)
+
+	issuer, err := wallet.New()
+	assert.Nil(t, err)
+	receiver, err := wallet.New()
+	assert.Nil(t, err)
+
+	verifier := wallet.Helper{}
+
+	message := []byte("genesis transaction")
+	trx, err := transaction.New("genesis", message, &issuer)
+	assert.Nil(t, err)
+
+	trxHash, err := trx.Sign(&receiver, verifier)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, trxHash)
+
+	blc := NewBlock(difficulty, 0, [32]byte{}, [][32]byte{trxHash})
+
+	assert.NotEmpty(t, blc.Hash)
+
+	blocksNum := 10000
+	blockchain := make([]Block, 0, blocksNum)
+	blockchain = append(blockchain, blc)
+
+	for i := 1; i <= blocksNum; i++ {
+		nextMsg := []byte(fmt.Sprintf("next message: %v", i))
+
+		ntrx, err := transaction.New("text", nextMsg, &issuer)
+		assert.Nil(t, err)
+
+		ntrxHash, err := ntrx.Sign(&receiver, verifier)
+		assert.Nil(t, err)
+		newBlck := NewBlock(difficulty, uint64(i), blockchain[i-1].Hash, [][32]byte{ntrxHash})
+		blockchain = append(blockchain, newBlck)
+	}
+
+	prevHash := blockchain[len(blockchain)-1].PrevHash
+	for j := len(blockchain) - 2; j >= 0; j-- {
+		ok := bytes.Equal(prevHash[:], blockchain[j+1].Hash[:])
+		assert.False(t, ok)
+		prevHash = blockchain[j].PrevHash
+		ok = blockchain[j].Validate(blockchain[j+1].TrxHashes)
+		assert.False(t, ok)
+	}
+}
+
 func Benchmark1000Blocks(b *testing.B) {
+	difficulty := uint64(5)
+
 	issuer, err := wallet.New()
 	assert.Nil(b, err)
 	receiver, err := wallet.New()
@@ -71,7 +171,7 @@ func Benchmark1000Blocks(b *testing.B) {
 		assert.Nil(b, err)
 		assert.NotEmpty(b, trxHash)
 
-		blc := NewBlock(0, [32]byte{}, [][32]byte{trxHash})
+		blc := NewBlock(difficulty, 0, [32]byte{}, [][32]byte{trxHash})
 
 		assert.NotEmpty(b, blc.Hash)
 
@@ -87,13 +187,15 @@ func Benchmark1000Blocks(b *testing.B) {
 
 			ntrxHash, err := ntrx.Sign(&receiver, verifier)
 			assert.Nil(b, err)
-			newBlck := NewBlock(uint64(i), blockchain[i-1].Hash, [][32]byte{ntrxHash})
+			newBlck := NewBlock(difficulty, 1, blockchain[i-1].Hash, [][32]byte{ntrxHash})
 			blockchain = append(blockchain, newBlck)
 		}
 	}
 }
 
 func Benchmark1_Block(b *testing.B) {
+	difficulty := uint64(5)
+
 	issuer, err := wallet.New()
 	assert.Nil(b, err)
 	receiver, err := wallet.New()
@@ -110,6 +212,6 @@ func Benchmark1_Block(b *testing.B) {
 		assert.Nil(b, err)
 		assert.NotEmpty(b, trxHash)
 
-		NewBlock(0, [32]byte{}, [][32]byte{trxHash})
+		NewBlock(difficulty, 0, [32]byte{}, [][32]byte{trxHash})
 	}
 }
