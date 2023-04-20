343e4adf3212970a70798b872444e41935cb7ab9
Bartosz Lenart <lenart.consulting@gmail.com>
2023-04-20 13:10:50 +0200

    Generate README.md from the code documentation. (#37)

--- gendocs.sh
+++ gendocs.sh
@@ -2,5 +2,7 @@
 
 gomarkdoc ./... > docs.md
 cat docs/header.txt docs.md > docs/docs.md
+echo "docs generated web page"
+cat docs/git_hub_header.txt docs.md > README.md
+echo "docs generated README.md"
 rm docs.md
-echo "docs generated"
