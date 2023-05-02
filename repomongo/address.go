5b4aae7188f6099f1c7eec87b8bf346264342815
Bartosz Lenart <lenart.consulting@gmail.com>
2023-05-02 10:16:35 +0200

    Postgresql address queries.

--- repomongo/address.go
+++ repomongo/address.go
@@ -4,29 +4,24 @@ import (
 	"context"
 	"errors"
 
+	"github.com/bartossh/Computantis/address"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// Address holds information about unique PublicKey.
-type Address struct {
-	ID        any    `json:"-"          bson:"_id,omitempty" db:"id"`
-	PublicKey string `json:"public_key" bson:"public_key"    db:"public_key"`
-}
-
 // WriteAddress writes unique address to the database.
-func (db DataBase) WriteAddress(ctx context.Context, address string) error {
-	if address == "" {
+func (db DataBase) WriteAddress(ctx context.Context, addr string) error {
+	if addr == "" {
 		return errors.New("public key is empty")
 	}
 
-	addr := Address{
+	a := address.Address{
 		ID:        primitive.NewObjectID(),
-		PublicKey: address,
+		PublicKey: addr,
 	}
 
-	if _, err := db.inner.Collection(addressesCollection).InsertOne(ctx, addr); err != nil {
+	if _, err := db.inner.Collection(addressesCollection).InsertOne(ctx, a); err != nil {
 		return err
 	}
 	return nil
@@ -34,12 +29,12 @@ func (db DataBase) WriteAddress(ctx context.Context, address string) error {
 
 // CheckAddressExists checks if address exists in the database.
 // Returns true if exists and error if database error different from ErrNoDocuments.
-func (db DataBase) CheckAddressExists(ctx context.Context, address string) (bool, error) {
-	if address == "" {
+func (db DataBase) CheckAddressExists(ctx context.Context, addr string) (bool, error) {
+	if addr == "" {
 		return false, errors.New("public key is empty")
 	}
 
-	res := db.inner.Collection(addressesCollection).FindOne(ctx, bson.M{"public_key": address})
+	res := db.inner.Collection(addressesCollection).FindOne(ctx, bson.M{"public_key": addr})
 	if res.Err() != nil {
 		if errors.Is(res.Err(), mongo.ErrNoDocuments) {
 			return false, nil
