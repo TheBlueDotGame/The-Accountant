b76ea6a188cfd10773e5b74dfa9c8c1c46ce1352
Bartosz Lenart <lenart.consulting@gmail.com>
2023-05-11 09:20:24 +0200

    Fix docs.

--- gendocs.sh
+++ gendocs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gomarkdoc ./... > docs.md
-cat docs/header.txt docs/git_hub_header.md docs.md docs.md > docs/docs.md
+cat docs/header.txt docs.md docs.md > docs/docs.md
 echo "docs generated web page"
 cat docs/git_hub_header.md docs.md > README.md
 echo "docs generated README.md"
