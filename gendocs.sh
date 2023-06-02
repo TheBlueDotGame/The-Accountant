43f1702b813f3df1369e5cb3fb224cbe0d67288b
Bartosz Lenart <lenart.consulting@gmail.com>
2023-06-02 18:30:56 +0200

    Remove docs. (#133)

--- gendocs.sh
+++ gendocs.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 gomarkdoc ./... > docs.md
-cat docs/header.txt docs.md docs.md > docs/docs.md
-echo "docs generated web page"
-cat docs/git_hub_header.md docs.md > README.md
+cat header.md docs.md > README.md
 echo "docs generated README.md"
 rm docs.md
