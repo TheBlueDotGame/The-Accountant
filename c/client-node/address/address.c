339073c7e633ac5fa95eb421b3c4049c01eb8dec
Bartossh <lenart.consulting@gmail.com>
2023-08-29 14:55:43 +0200

    Create config reader that reads client node configuration from the file.

--- c/client-node/address/address.c
+++ c/client-node/address/address.c
@@ -18,7 +18,7 @@ char *encode_address_from_raw(unsigned char *raw, size_t len)
 {
     if (len != PUBLIC_KEY_LEN)
     {
-        printf("public private key length is not valid, expected: [ %i ], got: [ %zi ]\n", PUBLIC_KEY_LEN, len);
+        printf("Public private key length is not valid, expected: [ %i ], got: [ %zi ]\n", PUBLIC_KEY_LEN, len);
         exit(1);
     }
 
@@ -26,14 +26,14 @@ char *encode_address_from_raw(unsigned char *raw, size_t len)
     char *b58 = malloc(sizeof(char)*b58_len);
     if (b58 == NULL)
     {
-        printf("failed to allocate [ %li ] bytes\n", b58_len);
+        printf("Failed to allocate [ %li ] bytes\n", b58_len);
         exit(1);
     }
     
     bool ok = b58enc(b58, &b58_len, (void *)raw, len);
     if (!ok)
     {
-        printf("base58 encoding faild\n");
+        printf("Base58 encoding faild\n");
         exit(1);
     }
 
@@ -46,14 +46,14 @@ int decode_address_to_raw(char *str, unsigned char **raw)
     *raw = malloc(sizeof(unsigned char)*len);
     if (*raw == NULL)
     {
-        printf("failed to allocate [ %li ] bytes\n", len);
+        printf("Failed to allocate [ %li ] bytes\n", len);
         exit(1);
     }
     
     bool ok = b58tobin(*raw, &len, str, strlen(str));
     if (!ok)
     {
-        printf("base58 decoding faild\n");
+        printf("Base58 decoding faild\n");
         exit(1);
     }
 
