1d9068b3e6c1bedd4781a10f845759dc4433412e
Bartossh <lenart.consulting@gmail.com>
2023-09-06 14:15:53 +0200

    Move print to single function.

--- cmd/wallet/main.go
+++ cmd/wallet/main.go
@@ -161,8 +161,7 @@ func run(action int, cfg fileoperations.Config) error {
 		if err := h.SaveToPem(&w); err != nil {
 			return err
 		}
-
-		pterm.Info.Printf("Wallet public address is [ %s ]\n", w.Address())
+		printWalletPublicAddress(w.Address())
 		return nil
 	case actionFromGobToPem:
 		h := fileoperations.New(cfg, aeswrapper.New())
@@ -173,7 +172,7 @@ func run(action int, cfg fileoperations.Config) error {
 		if err := h.SaveToPem(&w); err != nil {
 			return err
 		}
-		pterm.Info.Printf("Wallet public address is [ %s ]\n", w.Address())
+		printWalletPublicAddress(w.Address())
 		return nil
 
 	case actionFromPemToGob:
@@ -185,12 +184,16 @@ func run(action int, cfg fileoperations.Config) error {
 		if err := h.SaveWallet(&w); err != nil {
 			return err
 		}
-		pterm.Info.Printf("Wallet public address is [ %s ]\n", w.Address())
+		printWalletPublicAddress(w.Address())
 		return nil
 	default:
 		return errors.New("unimplemented action")
 
 	}
+}
+
+func printWalletPublicAddress(address string) {
+	pterm.Info.Printf("Wallet public address is [ %s ]\n", address)
 }
 
 func printSuccess() {
