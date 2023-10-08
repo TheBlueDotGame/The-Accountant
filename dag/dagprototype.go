10d64a486f2ebda98a8eaee9eff29e0193950471
Bartossh <lenart.consulting@gmail.com>
2023-10-08 14:52:45 +0200

    Extend test to 10 gossipers.

--- dag/dagprototype.go
+++ dag/dagprototype.go
@@ -101,8 +101,24 @@ func runGoshipPrototypeTest(print bool) {
 			n:      newNode(),
 			values: []int{50, 51, 52, 53, 54, 55, 56, 57, 58, 59},
 		},
+		{
+			n:      newNode(),
+			values: []int{60, 61, 62, 63, 64, 65, 66, 67, 68, 69},
+		},
+		{
+			n:      newNode(),
+			values: []int{70, 71, 72, 73, 74, 75, 76, 77, 78, 79},
+		},
+		{
+			n:      newNode(),
+			values: []int{80, 81, 82, 83, 84, 85, 86, 87, 88, 89},
+		},
+		{
+			n:      newNode(),
+			values: []int{90, 91, 92, 93, 94, 95, 96, 97, 98, 99},
+		},
 	}
-	upperValuesRange := 59
+	upperValuesRange := 99
 
 	ctx, cancel := context.WithCancel(context.Background())
 
