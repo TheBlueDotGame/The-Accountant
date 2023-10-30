b2184fb8c7e71be6bd5e02068e5e884be499683c
Bartosz Lenart <lenart.consulting@gmail.com>
2023-10-30 15:13:00 +0100

    263 dag trunk (#269)
    * 264 dag main structure and algorithm (#265)
    
    * Accounting and gossip protocols prototypes.
    
    * Tag prototype build.
    
    * Update messages.
    
    * Update info message.
    
    * Update messages and spacing.
    
    * Use number accumulator.
    
    * Extend test to 10 gossipers.
    
    * Cleanup space, remove redundant messages.
    
    * Rename package.
    
    * 266 dag db and binary encoding decoding (#267)
    
    * BadgerDB and fast binary encoding with proof.
    
    * Vertex functionalities.
    
    * Traverse the graph to validate the tip.
    
    * Refactor for in memory dag.
    
    * Add short memory for accountant and use last vertex if no leafs avaliable.
    
    * Set last hash in short memory.
    
    * Refactor leaf validation.
    
    * Validate and add to graph leaf from another node.
    
    * Upgrade packages to patched versions.
    
    * Allow leaves from trusted nodes to go without validation.
    
    * Calculate balance of public address.
    
    * Clear naming in accountant package.
    
    * Run Badger GC.
    
    * Fix contex done in select and create genesis.
    
    * Pass pointer to Vertex to dag and fix switch type accordingly.
    
    * Format code.
    
    * Simple accounting tests and benchmarks.
    
    * Guard concurrent validation and create concurrency tests and benchmarks.
    
    * Add balance to dag traverse register.
    
    * Update balance to replay with last hash if not rnough in channel.wa
    
    * Update readmy to match DAG and gossip about gossip protocols.
    
    * Accountant storage implementation.
    
    * Test accountant storage.
    
    * Validate trx exists and vertex exists.
    
    * Validate minimal weight.
    
    * Test multiple nodes and multiple producers balance consecutive vertex creation at random node.
    
    * Fix transaction validation on contract.
    
    * Fix spice stringify and spice uint64 overflow issue.

---
+++ accountant/config.go
@@ -0,0 +1,9 @@
+package accountant
+
+// Config contains configuration for the AccountingBook.
+type Config struct {
+	TrustedNodesDBPath       string `yaml:"trusted_nodes_db_path"`
+	TokensDBPath             string `yaml:"tokens_db_path"`
+	TraxsToVerticesMapDBPath string `yaml:"trxs_to_vertices_map_db_path"`
+	VerticesDBPath           string `yaml:"vertices_db_path"`
+}
